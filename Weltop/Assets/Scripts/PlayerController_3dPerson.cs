3686a6f9997e850486c6ce89663b9be64b2dce2d
mollydog25 <jackflan6@gmail.com>
2026-09-23 14:49:38 -0700

    smoothed the camera/ changed the camera object
    The camera was jittering

--- Weltop/Assets/Scripts/PlayerController_3dPerson.cs
+++ Weltop/Assets/Scripts/PlayerController_3dPerson.cs
@@ -1,29 +1,31 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController_3dPerson : MonoBehaviour
 {
     public float speed;
+    public float accel;
     private Rigidbody rb;
     private GameObject cam;
     private GameObject player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        cam = transform.parent.GetComponentInChildren<Camera>().gameObject;
+        cam = transform.parent.GetComponentInChildren<CinemachineCamera>().gameObject;
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector2 moveinput = InputSystem.actions["Move"].ReadValue<Vector2>();
         Vector3 move = new Vector3(cam.transform.forward.x,0,cam.transform.forward.z).normalized * moveinput.y
             + new Vector3(cam.transform.right.x,0, cam.transform.right.z).normalized * moveinput.x;
 
 
-        rb.linearVelocity = move * speed;
-
+        rb.AddForce(move * accel);
+        rb.maxLinearVelocity = speed;
 
     }
 }
